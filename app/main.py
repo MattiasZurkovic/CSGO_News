3679e3b9e5c2ce6647370d8d1860a95fbf30f835
MattiasZurkovic <mattias@zurkovic.com>
2015-07-24 20:26:20 +0200

    Add access tokens

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask import render_template
+import twitter
 
 app = Flask(__name__)
+
+# api = twitter.Api()
+
+t = Twitter(auth=OAuth(2834176217-coE5CGfxIdniddoou1HOBcG3r4KVdVG2UzJQStS, 3tfg6G4clDY42ie6wYekxf77xHGKCZjmWtUzIEqRTHqoW, aRahVNAuCVdWy5PGFjMoAIWui, fABUmGW1uV4pnlgTpwSx8KAxQdbVH6fz2le4dEW4e9wlnxmP2b))
 
 @app.route('/')
 def index():
@@ -9,7 +14,7 @@ def index():
 
 @app.route('/test_render')
 def test_render():
-    return render_template('test.html')
+    return t.statuses.user_timeline(screen_name="billybob")
 
 if __name__ == '__main__':
     app.run()
